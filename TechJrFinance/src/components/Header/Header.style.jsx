257b2fe2fb0ae998f8d4c4332d7e283df0200bd9
AlanSarasate <alansarasatecc@gmail.com>
2024-01-19 15:50:22 -0300

    style: Remoção dos media-querys e mudanças nos espaçamentos

--- TechJrFinance/src/components/Header/Header.style.jsx
+++ TechJrFinance/src/components/Header/Header.style.jsx
@@ -2,27 +2,11 @@ import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
     width: 100%;
+    max-height: 212px;
     background-color: ${function (props) {  return props.theme.colors.primaryColor } };
-    padding: 0 100px;
+    padding: 0 160px;
     display: flex;
     flex-direction: column;
-
-
-    @media (min-width: 100px){
-        height: 260px;
-    }
-    @media (min-width: 320px){
-        padding: 0 20px;
-        height: 220px;
-    }
-    @media (min-width: 720px){
-        padding: 0 50px;
-        height: 170px
-    }
-    @media (min-width: 900px){
-        padding: 0 100px;
-        height: 212px
-    }
 `
 
 export const LogoBox = styled.div`
@@ -31,35 +15,10 @@ export const LogoBox = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-
-    @media (max-width: 319px){
-        margin-top: 50px;
-        flex-direction: column;
-    }
-    @media (min-width: 320px){
-        margin-top: 50px;
-    }
-    @media (min-width: 720px){
-        margin-top: 40px;
-    }
-    @media (min-width: 900px){
-        margin-top: 32px;
-    }
+    margin: 32px 0 64px 0;
 
     img{
         height: 27px;
-        @media (max-width: 319px){
-            margin-bottom: 30px;
-        }
-        @media (min-width: 320px){
-            width: 150px;
-        }
-        @media (min-width: 720px){
-            width: 164px;
-        }
-        @media (min-width: 900px){
-            width: 186px;
-        }
     }
 `
 
@@ -77,22 +36,7 @@ export const NewTransitionButton = styled.button`
 
     &:hover{
         opacity: 0.8;
-    }
-
-    @media (min-width: 320px){
-        width: 130px;
-        font-size: 12px;
-    }
-    @media (min-width: 720px){
-        width: 170px;
-        font-size: 14px;
-    }
-    @media (min-width: 720px){
-        width: 195px;
-        font-size: 16px;
-        height: 44px;
-    }
-    
+    }  
 `
 
 export const CardsSection = styled.div`
@@ -101,11 +45,6 @@ export const CardsSection = styled.div`
     flex-direction: row;
     justify-content: space-between;
     position: relative;
-    top: 60px;
-
-    @media (min-width: 320px){
-        overflow-y: hidden;
-        gap: 30px;
-    }
+    gap: 32px;
 `
 
